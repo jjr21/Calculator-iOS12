d0c45305b6df5fa3a12c5a702e3df4ce4c66a9ed
jonathan rogers <jjrogers635@gmail.com>
2019-08-07 13:25:49 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+     private var statusButton: Bool = true
+    private var numberValue: String = ""
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        statusButton = true
+        numberValue = " "
+        guard let number = Double(displayLabel.text!) else{
+            fatalError("ERROR, cannot convert to double")
+        }
+        
+      
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if (statusButton == true){
+                numberValue =  numValue
+                displayLabel.text = numberValue
+                statusButton = false
+            }
+            else{
+                numberValue = numValue
+                displayLabel.text = displayLabel.text! + numberValue
+            }
+        }
+    }
+
+}
+
